36ff14cc847ac56633b12d216d01ec50e1be72d7
hk <764432054@qq.com>
2020-10-19 23:08:32 +0800

    exit

--- stu/chap-25/fork01.c
+++ stu/chap-25/fork01.c
@@ -2,17 +2,8 @@
 
 #define MAX_LINE 4096
 
-char
-rot13_char(char c) {
-    if ((c >= 'a' && c <= 'm') || (c >= 'A' && c <= 'M'))
-        return c + 13;
-    else if ((c >= 'n' && c <= 'z') || (c >= 'N' && c <= 'Z'))
-        return c - 13;
-    else
-        return c;
-}
 
-void child_run(int fd) {
+void child_run(int fd,int i) {
     char outbuf[MAX_LINE + 1];
     size_t outbuf_used = 0;
     ssize_t result;
@@ -21,6 +12,9 @@ void child_run(int fd) {
         char ch;
         result = recv(fd, &ch, 1, 0);
         if (result == 0) {
+//            exit( i);
+            printf("child pid===%d\n",getpid());
+            exit(getpid());
             break;
         } else if (result == -1) {
             perror("read");
@@ -29,7 +23,7 @@ void child_run(int fd) {
 
         /* We do this test to keep the user from overflowing the buffer. */
         if (outbuf_used < sizeof(outbuf)) {
-            outbuf[outbuf_used++] = rot13_char(ch);
+            outbuf[outbuf_used++] = ch;
         }
 
         if (ch == '\n') {
@@ -42,17 +36,31 @@ void child_run(int fd) {
 
 
 void sigchld_handler(int sig) {
-    while (waitpid(-1, 0, WNOHANG) > 0);
+    printf("sig=%d\n",sig);
+    pid_t wpid=0;
+    int  status;
+
+    while ((wpid=waitpid(-1, &status, WNOHANG)) > 0){
+        printf("wpid =%d exited,rawstatus=%d \n",wpid,status);
+        if(WIFEXITED(status)){
+            printf("normal exit status=%d\n",WEXITSTATUS(status));
+        }else{
+            printf("unnormal exit!\n");
+        }
+    }
     return;
 }
 
 int main(int c, char **v) {
     int listener_fd = tcp_server_listen(SERV_PORT);
+    printf("master pid=%d\n",getpid());
     signal(SIGCHLD, sigchld_handler);
+    int i=0;
     while (1) {
         struct sockaddr_storage ss;
         socklen_t slen = sizeof(ss);
         int fd = accept(listener_fd, (struct sockaddr *) &ss, &slen);
+        i++;
         if (fd < 0) {
             error(1, errno, "accept failed");
             exit(1);
@@ -60,7 +68,8 @@ int main(int c, char **v) {
 
         if (fork() == 0) {
             close(listener_fd);
-            child_run(fd);
+            printf("child pid=%d\n",getpid());
+            child_run(fd,i);
             exit(0);
         } else {
             close(fd);
@@ -69,3 +78,4 @@ int main(int c, char **v) {
 
     return 0;
 }
+//gcc fork01.c ../lib/tcp_server25.c -o fork01 -w
